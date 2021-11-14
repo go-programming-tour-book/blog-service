c88ac499cbc7cdef4eae6c82ed9233d4354d9e2f
CasperNan <szzqyhello@163.com>
2021-11-14 20:48:03 +0800

    fix middleware translations

--- global/validator.go
+++ global/validator.go
@@ -2,8 +2,11 @@ package global
 
 import (
 	"github.com/go-programming-tour-book/blog-service/pkg/validator"
+
+	ut "github.com/go-playground/universal-translator"
 )
 
 var (
 	Validator *validator.CustomValidator
+	Ut        *ut.UniversalTranslator
 )
--- internal/middleware/translations.go
+++ internal/middleware/translations.go
@@ -2,37 +2,18 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
-	"github.com/go-playground/locales/en"
-	"github.com/go-playground/locales/zh"
-	"github.com/go-playground/locales/zh_Hant_TW"
-	"github.com/go-playground/universal-translator"
-	validator "github.com/go-playground/validator/v10"
-	en_translations "github.com/go-playground/validator/v10/translations/en"
-	zh_translations "github.com/go-playground/validator/v10/translations/zh"
+	"github.com/go-programming-tour-book/blog-service/global"
 )
 
 func Translations() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		uni := ut.New(en.New(), zh.New(), zh_Hant_TW.New())
 		locale := c.GetHeader("locale")
-		trans, _ := uni.GetTranslator(locale)
-		v, ok := binding.Validator.Engine().(*validator.Validate)
-		if ok {
-			switch locale {
-			case "zh":
-				_ = zh_translations.RegisterDefaultTranslations(v, trans)
-				break
-			case "en":
-				_ = en_translations.RegisterDefaultTranslations(v, trans)
-				break
-			default:
-				_ = zh_translations.RegisterDefaultTranslations(v, trans)
-				break
-			}
+		trans, found := global.Ut.GetTranslator(locale)
+		if found {
 			c.Set("trans", trans)
+		} else {
+			c.Set("trans", "en")
 		}
-
 		c.Next()
 	}
 }
